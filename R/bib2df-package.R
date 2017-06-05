9cd6e2fd89358dce2129e01401009e1545f37e68
Philipp Ottolinger <philipp@ottolinger.de>
2017-06-05 22:29:23 +0200

    Add top-level documentation

---
+++ R/bib2df-package.R
@@ -0,0 +1,14 @@
+#' bib2df: Parse a BibTeX file to data.frame
+#'
+#' This package provides functions to parse and write BibTeX files. BibTeX files
+#' can be parsed to data.frame to make them accessible with popular tools like
+#' dplyr, tidyr, ggplot2 and many more.
+#'
+#' BibTeX entries represented in a data.frame can be altered in a familar way and
+#' written back to a valid BibTeX file.
+#'
+#' To learn more about bib2df, start with the vignettes:
+#'  \code{browseVignettes(package = "bib2df")}
+#' @docType package
+#' @name bib2df-package
+NULL
