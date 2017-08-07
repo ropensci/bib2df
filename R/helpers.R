04893674b0ca97a80fbef58cdf89b57259bc1a6a
Philipp Ottolinger <philipp@ottolinger.de>
2017-08-07 13:45:32 +0200

    moved internal helper functions to helpers.R

---
+++ R/helpers.R
@@ -0,0 +1,9 @@
+capitalize <- function(string) {
+  paste0(substr(string, 1, 1),
+         tolower(substr(string, 2, nchar(string) )))
+}
+
+na_replace <- function(df) {
+  df[is.na(df)] <- ""
+  return(df)
+}
