ff222303db3284bda15177c6bd9b443c6230b0f2
HedvigS <hedvig.skirgard@gmail.com>
2024-04-03 14:40:29 +0200

    Update bib2df_read.R

--- R/bib2df_read.R
+++ R/bib2df_read.R
@@ -3,5 +3,7 @@
 bib2df_read <- function(file) {
   bib <- readLines(file)
   bib <- str_replace_all(bib, "[^[:graph:]]", " ")
+  bib <- str_replace_all(bib, "=", " = ") # add desired spaces
+  bib <- str_replace_all(bib, "  ", " ")   # remove double spaces in case you have it
   return(bib)
 }
