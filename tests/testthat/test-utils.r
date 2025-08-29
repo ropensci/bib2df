db92a3fdfa0b99dc55a931dddabc521137f2fb9e
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2025-08-29 12:07:21 -0700

    Removes capitalize(), replace with tolower()
    Close #67

--- tests/testthat/test-utils.r
+++ tests/testthat/test-utils.r
@@ -1,10 +1,5 @@
 context("Helper functions")
 
-test_that("capitalize() works", {
-  expect_true(capitalize("TEST") == "Test")
-  expect_true(capitalize("Test") == "Test")
-})
-
 test_that("na_replace() works", {
   df <- data.frame(a = NA, b = 1)
   expect_true(na_replace(df)$a[1] == "")
